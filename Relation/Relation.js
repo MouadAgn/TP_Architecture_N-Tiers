8876ed758a06844cab20cd0efb9b1c4b017c1872
MouadAgn <103049879+MouadAgn@users.noreply.github.com>
2025-01-16 10:54:56 +0100

    Les Relations entre les models

---
+++ Relation/Relation.js
@@ -0,0 +1,19 @@
+const User = require('../BackEnd_User/Models/UserModel');
+const Publication = require('../BackEnd_Publication/Models/PublicationModel');
+
+// Relation User - Publication (One-to-Many)
+User.hasMany(Publication, {
+    foreignKey: 'userId',
+    as: 'publications',
+    onDelete: 'CASCADE'
+});
+
+Publication.belongsTo(User, {
+    foreignKey: 'userId',
+    as: 'user'
+});
+
+module.exports = {
+    User,
+    Publication
+}; 
